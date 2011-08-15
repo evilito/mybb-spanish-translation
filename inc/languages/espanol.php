f8943586f8ade20e44da3a07362568f0abead139
evilito <evil@clanuis.net>
2011-08-15 14:01:03 -0500

    package added

---
+++ inc/languages/espanol.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * MyBB 1.6 Spanish Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: espanol.php 5016 2010-12-16 12:28:33Z Anio_pke $
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "Español (Internacional)";
+
+// The author of the language
+$langinfo['author'] = "SoporteMyBB";
+
+// The language authors website
+$langinfo['website'] = "http://www.soportemybb.com/";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1601";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 1;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "es";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
+?>
---
+++ inc/languages/espanol/admin/config_badwords.lang.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * MyBB 1.6 Spanish Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: config_badwords.lang.php 5072 2011-07-31 18:47:42Z evilito $
+ */
+
+$l['bad_words'] = "Filtros de palabras";
+$l['edit_bad_word'] = "Editar filtro";
+$l['edit_bad_word_desc'] = "Aquí puedes editar los filtros y sus reemplazos.";
+$l['bad_word_filters'] = "Filtros de palabras";
+$l['bad_word_filters_desc'] = "Esta función te permite configurar una lista de palabras o frases que se reemplazarán automaticamente en los mensajes en tu foro. Es útil para filtrar insultos y palabras fuera de tono.";
+
+$l['bad_word'] = "Palabra";
+$l['bad_word_desc'] = "Introduce la palabra que deseas filtrar. El '*' representa cualquier caracter (a-zA-Z0-9_).";
+$l['bad_word_max'] = "Una palabra filtrada no puede superar los 100 caracteres.";
+$l['replacement'] = "Reemplazo";
+$l['replacement_desc'] = "Introduce la cadena que reemplazará a la palabra filtrada (Si se deja en blanco se mostrarán unos astericos)";
+$l['replacement_word_max'] = "Una palabra de reemplazo no puede superar los 100 caracteres.";
+$l['error_replacement_word_invalid'] = "Una palabra de reemplazo no puede ser equivalente a la palabra filtrada.";
+
+$l['save_bad_word'] = "Guardar filtro";
+$l['no_bad_words'] = "Actualmente no existe ningún filtro de palabras.";
+$l['add_bad_word'] = "Agregar filtro";
+
+$l['error_missing_bad_word'] = "No has introducido la palabra a filtrar.";
+$l['error_invalid_bid'] = "El filtro especificado no existe.";
+
+$l['success_added_bad_word'] = "El filtro se ha añadido correctamente.";
+$l['success_deleted_bad_word'] = "El filtro se ha eliminado correctamente.";
+$l['success_updated_bad_word'] = "El filtro se ha actualizado correctamente.";
+
+$l['confirm_bad_word_deletion'] = "¿Estás seguro de querer eliminar este filtro?";
+
+?>

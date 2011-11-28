7dee9db293310d577f98db28bd22ba7904d9c9ae
Anio-pke <anio.pke@gmail.com>
2011-11-28 13:21:49 +0100

    Regresado al original (error al actualizarlo en el commit anterior)

--- inc/languages/espanol/admin/forum_attachments.lang.php
+++ inc/languages/espanol/admin/forum_attachments.lang.php
@@ -2,251 +2,107 @@
 /**
  * MyBB 1.6 Spanish Language Pack
  * Copyright 2010 MyBB Group, All Rights Reserved
- * 
- * $Id: forum_management.lang.php 5557 2011-11-28 13:18:33Z Anio_pke $
+ *
+ * $Id: forum_attachments.lang.php 5016 2010-08-11 12:32:33Z Anio_pke $
  */
 
-$l['forum_management'] = "Configurar foro";
-$l['forum_management_desc'] = "Esta sección te permite configurar las categorías y los foros. Puedes configurar los permisos y los moderadores de un foro específico. Si quieres cambiar el orden de uno o más foros o categorías, asegúrate de que envías el formulario al final de la página.";
-$l['add_forum'] = "Agregar nuevo foro";
-$l['add_forum_desc'] = "Aquí puedes agregar un nuevo foro o categoría. Además puedes establecer los permisos iniciales para este foro.";
-$l['copy_forum'] = "Copiar foro";
-$l['copy_forum_desc'] = "Aquí puedes copiar la configuración o los permisos de un foro existente a otro o a un nuevo foro.";
-$l['forum_permissions'] = "Permisos";
-$l['forum_permissions_desc'] = "Aquí puedes modificar todos los permisos para un grupo en un foro concreto";
-$l['view_forum'] = "Ver foro";
-$l['view_forum_desc'] = "Aquí puedes ver los subforos, editar los permisos y agregar moderadores a tu foro.";
-$l['add_child_forum'] = "Agregar subforo";
-$l['edit_forum_settings'] = "Editar ajustes del foro";
-$l['edit_forum_settings_desc'] = "Aquí puedes editar los ajustes y los permisos de un foro existente.";
-$l['edit_forum'] = "Editar foro";
-$l['edit_mod'] = "Editar moderador";
-$l['edit_mod_desc'] = "Aquí puedes modificar los ajustes de un moderador.";
-$l['forum_moderators'] = "Moderadores del foro";
-$l['forum_permissions2'] = "Permisos del foro";
-$l['more_subforums'] = "y {1} más.";
+// Tabs
+$l['attachments'] = "Archivos adjuntos";
+$l['stats'] = "Estadísticas";
+$l['find_attachments'] = "Buscar adjuntos";
+$l['find_attachments_desc'] = "Usando el sistema de búsqueda de adjuntos puedes buscar archivos específicos que los usuarios tengan subidos en el foro. Introduce los criterios abajo. Todos los campos son opcionales y los criterios que estén vacíos no se aplicarán en la búsqueda.";
+$l['find_orphans'] = "Buscar adjuntos huérfanos";
+$l['find_orphans_desc'] = "Los adjuntos huérfanos son archivos adjuntos que por alguna razón se perdieron en la base de datos o en el sistema de ficheros. Esta utilidad te permite localizarlos y borrarlos.";
+$l['attachment_stats'] = "Estadísticas";
+$l['attachment_stats_desc'] = "Abajo hay una estadística general de los archivos adjuntos que están actualmente en tu foro.";
 
-$l['manage_forums'] = "Foros";
-$l['forum'] = "Foro";
-$l['order'] = "Orden";
+// Errors
+$l['error_nothing_selected'] = "Por favor, selecciona uno o más adjuntos para borrar.";
+$l['error_no_attachments'] = "Actualmente no hay adjuntos en tu foro. En cuanto se suba algún archivo podrás acceder a esta sección.";
+$l['error_not_all_removed'] = "Solo se han podido eliminar algunos adjuntos correctamente, los demás no se han eliminado de la carpeta 'uploads'.";
+$l['error_invalid_username'] = "El nombre de usuario introducido es inválido.";
+$l['error_invalid_forums'] = "Uno o más de los foros seleccionados son inválidos.";
+$l['error_no_results'] = "No se han encontrado adjuntos con los criterios de búsqueda especificados.";
+$l['error_not_found'] = "El archivo adjunto no se ha encontrado en el directorio 'uploads'.";
+$l['error_not_attached'] = "El adjunto se ha subido hace 24 horas pero no se ha adjuntado al mensaje.";
+$l['error_does_not_exist'] = "El tema o el mensaje de este adjuntos no existe.";
 
-$l['subforums'] = "Subforos";
-$l['moderators'] = "Moderadores";
-$l['permissions'] = "Permisos";
-$l['delete_forum'] = "Eliminar foro";
+// Success
+$l['success_deleted'] = "Los adjuntos seleccionados se han eliminado correctamente";
+$l['success_orphan_deleted'] = "Los adjuntos huérfanos seleccionados se han eliminado correctamente";
+$l['success_no_orphans'] = "Actualmente no hay ningún adjunto huérfano en tu foro";
 
-$l['sub_forums'] = "Subforos";
-$l['update_forum_orders'] = "Guardar orden";
-$l['update_forum_permissions'] = "Guardar permisos";
-$l['reset'] = "Restablecer";
-$l['in_forums'] = "Foros en \"{1}\"";
-$l['forum_permissions_in'] = "Permisos de \"{1}\"";
-$l['moderators_assigned_to'] = "Moderadores de \"{1}\"";
-$l['edit_permissions'] = "Editar permisos";
-$l['set_permissions'] = "Establecer permisos";
-$l['using_custom_perms'] = "Usando permisos personalizados";
-$l['using_default_perms'] = "Usando permisos predeterminados";
-$l['clear_custom_perms'] = "Limpiar permisos personalizados";
-$l['set_custom_perms'] = "Ajustar permisos personalizados";
+// Confirm
+$l['confirm_delete'] = "¿Estás seguro de querer eliminar los ajduntos seleccionados?";
 
-$l['permissions_use_group_default'] = "Por defecto";
-$l['permissions_group'] = "Grupo";
-$l['permissions_canview'] = "¿Puede ver?";
-$l['permissions_canpostthreads'] = "¿Puede crear temas?";
-$l['permissions_canpostreplys'] = "¿Puede enviar respuestas?";
-$l['permissions_canpostpolls'] = "¿Puede crear encuestas?";
-$l['permissions_canuploadattachments'] = "¿Puede adjuntar archivos?";
-$l['permissions_all'] = "¿Todo?";
-
-$l['overview_allowed_actions'] = "Información: Acciones permitidas";
-$l['overview_disallowed_actions'] = "Información: Acciones no permitidas";
-$l['perm_drag_canview'] = "&#149; Ver";
-$l['perm_drag_canpostthreads'] = "&#149; Crear temas";
-$l['perm_drag_canpostreplys'] = "&#149; Responder mensajes";
-$l['perm_drag_canpostpolls'] = "&#149; Crear encuestas";
-
-$l['moderator_permissions'] = "Permisos del moderador";
-$l['forum_desc'] = "Configurar moderador del foro.";
-$l['edit_mod_for'] = "Editar opciones de moderador para \"{1}\"";
-$l['can_edit_posts'] = "¿Puede editar mensajes?";
-$l['can_delete_posts'] = "¿Puede eliminar mensajes?";
-$l['can_view_ips'] = "¿Puede ver IPs?";
-$l['can_open_close_threads'] = "¿Puede abrir/cerrar temas?";
-$l['can_manage_threads'] = "¿Puede moderar temas (Dividir, mover, copiar, combinar)?";
-$l['can_move_to_other_forums'] = "¿Puede mover temas a foros que este usuario no modera?";
-$l['save_mod'] = "Guardar moderador";
-
-$l['no_forums'] = "Actualmente no hay foros.";
-$l['no_moderators'] = "Actualmente no hay moderadores.";
-
-$l['success_forum_disporder_updated'] = "El orden de muestra se ha actualizado correctamente.";
-$l['success_forum_deleted'] = "El foro especificado se ha eliminado correctamente.";
-$l['success_moderator_deleted'] = "El moderador especificado se ha eliminado correctamente.";
-$l['success_forum_permissions_updated'] = "Los permisos del foro se han actualizado correctamente.";
-$l['success_forum_updated'] = "Los ajustes del foro se han actualizado correctamente.";
-$l['success_moderator_updated'] = "El moderador seleccionado se ha actualizado correctamente.";
-$l['success_custom_permission_cleared'] = "Los permisos personalizados para este foro se han limpiado correctamente.";
-
-$l['error_invalid_forum'] = "Por favor, selecciona un foro válido.";
-$l['error_invalid_moderator'] = "Por favor, selecciona un moderador válido para eliminarlo.";
-$l['error_invalid_fid'] = "La ID de foro seleccionada es inválida.";
-$l['error_forum_parent_child'] = "No puedes establecer como subforo a un foro padre como hijo de uno de sus subforos.";
-$l['error_forum_parent_itself'] = "El foro padre no puede ser subforo de sí mismo.";
-$l['error_incorrect_moderator'] = "Por favor, selecciona un moderador válido.";
-
-$l['confirm_moderator_deletion'] = "¿Estás seguro de querer eliminar este moderador de este foro?";
-$l['confirm_forum_deletion'] = "¿Estás seguro de querer eliminar este foro?";
-$l['confirm_clear_custom_permission'] = "¿Estás seguro de querer eliminar este permiso personalizado?";
-
-$l['forum_type'] = "Tipo de foro";
-$l['forum_type_desc'] = "Selecciona el tipo de foro que estás creando - un foro en el que puedes escribir o una categoría para contener otros foros.";
-$l['category'] = "Categoría";
-$l['title'] = "Título";
-$l['description'] = "Descripción";
-$l['save_forum'] = "Guardar foro";
-$l['parent_forum'] = "Foro padre";
-$l['parent_forum_desc'] = "Foro que contiene este subforo. Las categorías no necesitan foro padre - en este caso, selecciona 'Ninguno' - de todas formas, las categorías pueden tener un foro padre.";
-$l['none'] = "Ninguno";
-$l['display_order'] = "Orden";
-
-$l['show_additional_options'] = "Mostrar opciones adicionales";
-$l['hide_additional_options'] = "Ocultar opciones adicionales";
-$l['additional_forum_options'] = "Opciones adicionales";
-$l['forum_link'] = "Enlace";
-$l['forum_link_desc'] = "Para hacer una redirección a otra dirección, introduce la URL del destino al que quieres redireccionar. Al introducir una URL este campos desactiva la funcionalidad de foro; de todas formas puedes establecer permisos.";
-$l['forum_password'] = "Contraseña";
-$l['forum_password_desc'] = "Para proteger este foro, puedes elejir una contraseña que debe ser introducida para acceder. Nota: Los grupos necesitan los permisos para acceder a este foro.";
-$l['access_options'] = "Opciones de acceso";
-$l['forum_is_active'] = "¿Foro activo?";
-$l['forum_is_active_desc'] = "Si está deseleccionado, este foro no se mostrará a los usuarios y es como si no \"existiese\".";
-$l['forum_is_open'] = "¿Foro abierto?";
-$l['forum_is_open_desc'] = "Si está deseleccionado, los usuarios no podrán escribir en este foro, sin importar sus permisos.";
-
-$l['copy_to_new_forum'] = "Copiar a un nuevo foro";
-$l['source_forum'] = "Foro fuente";
-$l['source_forum_desc'] = "Foro del que se van a copiar los ajustes y/o los permisos.";
-$l['destination_forum'] = "Foro destino";
-$l['destination_forum_desc'] = "Foro al que se van a copiar los ajustes y/o los permisos.";
-$l['new_forum_settings'] = "Nueva configuración de foro";
-$l['copy_settings_and_properties'] = "Copiar la configuración del foro y las propiedades";
-$l['copy_settings_and_properties_desc'] = "Solo se aplica si existe el foro destino.";
-$l['copy_user_group_permissions'] = "Copiar permisos de los grupos de usuarios";
-$l['copy_user_group_permissions_desc'] = "Usar CTRL para seleccionar múltiples grupos.";
-
-$l['moderation_options'] = "Opciones de moderación";
-$l['mod_new_posts'] = "Si, moderar nuevos mensajes";
-$l['mod_new_threads'] = "Si, moderar nuevos temas";
-$l['mod_new_attachments'] = "Si, moderar nuevos archivos adjuntos";
-$l['mod_after_edit'] = "Si, moderar los mensajes antes de ser editados";
-$l['override_user_style'] = "Si, ignorar el estilo seleccionado por el usuario en este foro";
-$l['style_options'] = "Opciones de estilo";
-$l['forum_specific_style'] = "Estilo específico:";
-$l['use_default'] = "Usar estilo por defecto";
-$l['dont_display_rules'] = "No mostrar las reglas para este foro";
-$l['display_rules_inline'] = "Mostrar las reglas para este foro en la lista de temas";
-$l['display_rules_link'] = "Mostrar un enlace a las reglas para este foro";
-$l['display_method'] = "Método de muestra:";
-$l['rules'] = "Reglas:";
-$l['forum_rules'] = "Reglas del foro";
-$l['name'] = "Nombre";
+// == Pages
+// = Stats
+$l['general_stats'] = "Estadísticas generales";
+$l['stats_attachment_stats'] = "Adjuntos - Estadísticas";
+$l['num_uploaded'] = "<strong>Nº ajuntos subidos</strong>";
+$l['space_used'] = "<strong>Espacio en disco</strong>";
+$l['bandwidth_used'] = "<strong>Transferecia estimada</strong>";
+$l['average_size'] = "<strong>Tamaño medio de los adjuntos</strong>";
+$l['size'] = "Tamaño";
+$l['posted_by'] = "Subido por";
+$l['thread'] = "Tema";
+$l['downloads'] = "Descargas";
+$l['date_uploaded'] = "Fecha de subida";
+$l['popular_attachments'] = "Top 5 - Los adjuntos más populares";
+$l['largest_attachments'] = "Top 5 - Los adjuntos más grandes";
+$l['users_diskspace'] = "Top 5 - Usuarios que usan más espacio de disco";
 $l['username'] = "Nombre de usuario";
-$l['moderator_username_desc'] = "Nombre de usuario del moderador que quieres agregar";
-$l['add_user_as_moderator'] = "Agregar usuario como moderador";
-$l['usergroup'] = "Grupo de usuarios";
-$l['add_usergroup_as_moderator'] = "Agregar grupo de usuarios como moderadores";
-$l['moderator_usergroup_desc'] = "Título del grupo de usuarios que se agregará";
-$l['add_usergroup_moderator'] = "Agregar grupo como moderador";
-$l['add_user_moderator'] = "Agregar usuario como moderador";
+$l['total_size'] = "Tamaño";
 
-$l['default_view_options'] = "Opciones de vista por defecto";
-$l['default_date_cut'] = "Cortar fecha por defecto";
-$l['default_sort_by'] = "Ordenación por defecto";
-$l['default_sort_order'] = "Orden por defecto";
+// = Orphans
+$l['orphan_results'] = "Búsqueda de adjuntos huérfanos - Resultados";
+$l['orphan_attachments_search'] = "Búsqueda de adjuntos huérfanos";
+$l['reason_orphaned'] = "Razón";
+$l['reason_not_in_table'] = "No está en la tabla 'adjuntos'";
+$l['reason_file_missing'] = "Falta el archivo";
+$l['reason_thread_deleted'] = "Tema borrado";
+$l['reason_post_never_made'] = "Mensaje no creado";
+$l['unknown'] = "Desconocido";
+$l['results'] = "Resultados";
+$l['step1'] = "Paso 1";
+$l['step2'] = "Paso 2";
+$l['step1of2'] = "Paso 1 de 2 - Escaneando el sistema de ficheros";
+$l['step2of2'] = "Paso 2 de 2 - Escaneando base de datos";
+$l['step1of2_line1'] = "Por favor, espere, el sistema de ficheros está siendo escaneado buscando adjuntos huérfanos.";
+$l['step2of2_line1'] = "Por favor, espere, la base de datos está siendo escaneada buscando adjuntos huérfanos.";
+$l['step_line2'] = "Cuando el proceso se complete se saltará al siguiente paso automáticamente.";
 
-$l['board_default'] = "Por defecto del foro";
+// = Attachments / Index
+$l['index_find_attachments'] = "Adjuntos - Buscar adjuntos";
+$l['find_where'] = "Buscar adjuntos aquí...";
+$l['name_contains'] = "El nombre del archivo contiene";
+$l['name_contains_desc'] = "Para buscar con una sustitución introducir *.[extensión]. Ejemplo: *.zip.";
+$l['type_contains'] = "El tipo de archivo contiene";
+$l['forum_is'] = "El foro es";
+$l['username_is'] = "El nombre de usuario es";
+$l['more_than'] = "Más de";
+$l['greater_than'] = "Mayor que";
+$l['is_exactly'] = "Exactamente";
+$l['less_than'] = "Menor que";
+$l['date_posted_is'] = "La fecha de subida es";
+$l['days_ago'] = "días";
+$l['file_size_is'] = "El tamaño del archivo es";
+$l['kb'] = "KB";
+$l['download_count_is'] = "El número de descargas es";
+$l['display_options'] = "Opciones";
+$l['filename'] = "Nombre";
+$l['filesize'] = "Tamaño";
+$l['download_count'] = "Número de descargas";
+$l['post_username'] = "Nombre de usuario";
+$l['asc'] = "Ascendente";
+$l['desc'] = "Descendente";
+$l['sort_results_by'] = "Ordenar resultados por";
+$l['results_per_page'] = "Resultados por página";
+$l['in'] = "en orden";
 
-$l['datelimit_1day'] = "Último día";
-$l['datelimit_5days'] = "Últimos 5 días";
-$l['datelimit_10days'] = "Últimos 10 días";
-$l['datelimit_20days'] = "Últimos 20 días";
-$l['datelimit_50days'] = "Últimos 50 días";
-$l['datelimit_75days'] = "Últimos 75 días";
-$l['datelimit_100days'] = "Últimos 100 días";
-$l['datelimit_lastyear'] = "Último año";
-$l['datelimit_beginning'] = "Desde el principio";
-
-$l['sort_by_subject'] = "Título del tema";
-$l['sort_by_lastpost'] = "Fecha del último mensaje";
-$l['sort_by_starter'] = "Autor del tema";
-$l['sort_by_started'] = "Fecha de creación del tema";
-$l['sort_by_rating'] = "Calificación del tema";
-$l['sort_by_replies'] = "Número de respuestas";
-$l['sort_by_views'] = "Número de vistas";
-
-$l['sort_order_asc'] = "Ascendente";
-$l['sort_order_desc'] = "Descendente";
-
-$l['misc_options'] = "Opciones misceláneas";
-$l['allow_html'] = "Si, permitir HTML en los mensajes";
-$l['allow_mycode'] = "Si, permitir MyCode en los mensajes";
-$l['allow_smilies'] = "Si, permitir iconos gestuales en los mensajes";
-$l['allow_img_code'] = "Si, permitir código [img] en los mensajes (MyCode debe estar activado)";
-$l['allow_video_code'] = "Si, permitir código [videos] en los mensajes (MyCode debe estar activado)";
-$l['allow_post_icons'] = "Si, permitir seleccionar iconos para los mensajes";
-$l['allow_thread_ratings'] = "Si, permitir que los temas sean calificados";
-$l['show_forum_jump'] = "Si, mostrar en este foro el menú 'Salto de foro'";
-$l['use_postcounts'] = "Si, los mensajes enviados a este foro incrementan el contador de mensajes";
-
-$l['use_permissions'] = "Usar permisos";
-$l['use_permissions_desc'] = "Selecciona los permisos que quieres para este grupo de usuario - permisos heredados (eliminará los permisos personalizados) o permisos personalizados.";
-$l['inherit_permissions'] = "Usar los permisos del grupo o los permisos heredados del foro padre";
-$l['custom_permissions'] = "Usar permisos personalizados (Abajo)";
-$l['custom_permissions_for'] = "Permisos personalizados para";
-
-$l['inherited_permission'] = "heredado";
-$l['custom_permission'] = "personalizado";
-
-$l['save_permissions'] = "Guardar permisos";
-
-$l['error_missing_title'] = "No has introducido un título.";
-$l['error_no_parent'] = "No has seleccionado un foro padre.";
-$l['error_not_empty'] = "Los foros con temas no se pueden convertir en categorías.";
-
-$l['success_forum_added'] = "El foro se ha agregado correctamente.";
-$l['success_moderator_added'] = "El moderador se ha agregado al foro correctamente.";
-$l['success_forum_permissions_saved'] = "Los permisos del foro se han guardado correctamente.";
-$l['success_forum_copied'] = "El foro seleccionado se ha copiado correctamente.";
-
-$l['error_moderator_already_added'] = "El moderador especificado ya es moderador de este foro.";
-$l['error_moderator_not_found'] = "El nombre de usuario especificado no se encuentra.";
-$l['error_new_forum_needs_name'] = "Debes darle un nombre al nuevo foro.";
-$l['error_invalid_source_forum'] = "El foro fuente es inválido.";
-$l['error_invalid_destination_forum'] = "El foro destino es inválido.";
-
-$l['group_viewing'] = "Ver";
-$l['group_posting_rating'] = "Escribir / Calificar";
-$l['group_editing'] = "Ediciones";
-$l['group_polls'] = "Encuestas";
-$l['group_misc'] = "Misceláneos";
-
-$l['viewing_field_canview'] = "¿Puede ver el foro?";
-$l['viewing_field_canviewthreads'] = "¿Puede ver los temas del foro?";
-$l['viewing_field_canonlyviewownthreads'] = "¿Puede ver únicamente sus propios temas?";
-$l['viewing_field_candlattachments'] = "¿Puede descargar adjuntos?";
-
-$l['posting_rating_field_canpostthreads'] = "¿Puede crear temas?";
-$l['posting_rating_field_canpostreplys'] = "¿Puede enviar respuestas?";
-$l['posting_rating_field_canpostattachments'] = "¿Puede adjuntar archivos?";
-$l['posting_rating_field_canratethreads'] = "¿Puede calificar temas?";
-
-$l['editing_field_caneditposts'] = "¿Puede editar sus mensajes?";
-$l['editing_field_candeleteposts'] = "¿Puede eliminar sus mensajes?";
-$l['editing_field_candeletethreads'] = "¿Puede eliminar sus temas?";
-$l['editing_field_caneditattachments'] = "¿Puede editar sus adjuntos?";
-
-$l['polls_field_canpostpolls'] = "¿Puede crear encuestas?";
-$l['polls_field_canvotepolls'] = "¿Puede votar en las encuestas?";
-
-$l['misc_field_cansearch'] = "¿Puede buscar en el foro?";
+// Buttons
+$l['button_delete_orphans'] = "Eliminar huérfanos seleccionados";
+$l['button_delete_attachments'] = "Eliminar adjuntos seleccionados";
+$l['button_find_attachments'] = "Buscar adjuntos";
 
 ?>

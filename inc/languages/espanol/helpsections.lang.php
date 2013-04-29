b0f3d1d8dcf076fa6fcfe07728e370adbb3bb4df
Anio-pke <anio.pke@gmail.com>
2013-04-30 00:09:42 +0300

    Actualizado para MyBB 1.6.10

--- inc/languages/espanol/helpsections.lang.php
+++ inc/languages/espanol/helpsections.lang.php
@@ -3,8 +3,11 @@
  * MyBB 1.6 Spanish Language Pack
  * Copyright 2010 MyBB Group, All Rights Reserved
  * 
- * $Id: helpsections.lang.php 5016 2010-08-10 12:32:33Z Anio_pke $
+ * $Id: helpsections.lang.php 5016 2013-04-29 12:32:33Z Anio_pke $
  */
+
+/* Descomenta las líneas de abajo para usar estas cadenas en lugar de los documentos de ayuda de la base de datos. */
+
 
 // Help Section 1
 $l['s1_name'] = "Mantenimiento para usuarios";
@@ -13,4 +16,6 @@ $l['s1_desc'] = "Instrucciones básicas para el mantenimiento de una cuenta en e
 // Help Section 2
 $l['s2_name'] = "Participación";
 $l['s2_desc'] = "Participar, responder, y otras instrucciones básicas del uso del foro.";
+
 ?>
+
